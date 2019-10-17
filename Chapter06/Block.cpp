64ecfe69780151cc4a655e92da20e70937f72f03
Haruki Okagawa <hw17a036@oecu.jp>
2019-10-17 18:49:58 +0900

    正味変わってない

--- Chapter06/Block.cpp
+++ Chapter06/Block.cpp
@@ -15,7 +15,7 @@ Block::Block()
     :Actor()
 {
     mMeshComp = new MeshComponent(this);
-    mMeshComp->SetMesh(_gameInstance -> GetRenderer() ->GetMesh("Assets/Cube.obj"));
+    mMeshComp->SetMesh(_gameInstance -> GetRenderer() ->GetMesh("Assets/Cube.gpmesh"));
 }
 
 void Block::UpdateActor(float deltaTime)
