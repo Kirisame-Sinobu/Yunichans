c85f0831b1b23f5f1f5a4b17f95cdd0e727a7ac5
Haruki Okagawa <hw17a036@oecu.jp>
2019-10-24 15:23:47 +0900

    しゃあなし変えてあげたわ

--- Chapter06/Game.cpp
+++ Chapter06/Game.cpp
@@ -176,7 +176,7 @@ void Game::LoadData()
         for(int j = 0; j < 10; j++){
             Block* block = new Block();
             block->SetPosition(Vector3(500 - (101.0f * i), 500 - (101.0f * j), 200.0f));
-            block->SetScale(50.0f);
+            block->SetScale(100.0f);
             Quaternion q(Vector3::UnitY, -Math::Pi);
             q = Quaternion::Concatenate(q, Quaternion(Vector3::UnitZ, Math::Pi));
             block->SetRotation(q);
